8c837ac678e5dc1997e1092a33d16d42fe9e35ae
Stephanie Sison <sjsison27@icloud.com>
2022-06-30 11:59:02 -0700

    commit tree f1e9efaf40ac844ff8e7768149ede5d403768e8c

--- main.js
+++ main.js
@@ -1,10 +1,67 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+//putting this here so I can have global scope access
+const errorModal = document.querySelector("#modal")
+
+//Add the .hidden class to the error modal in the HTML so it does not appear when the page first load
+errorModal.classList.add("hidden")
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Content Has Loaded")
+ 
+//call clickHeartListener()
+clickHeartListener()
 
+})
 
+// When a user clicks on an empty heart (This is one way to do it, you then call it in the above function)
+// function likeHeart() {
+//   const likeHeart = document.querySelectorAll(".like-glyph")
+
+//   likeHeart.forEach((oneLike) => {
+//     oneLike.addEventListener("click",() => console.log("LIKE!"))
+//   })
+//   }
+
+// When a user clicks on an empty heart:
+function clickHeartListener() {
+  document.addEventListener('click', (event) => {
+    if (event.target.classList[0] === 'like-glyph'){
+// Invoke mimicServerCall to simulate making a server request
+    mimicServerCall()
+//When the "server" returns a success status:
+     .then((resp) => {
+        const activated = event.target.classList.contains("activated-heart");
+
+        if (activated) {
+//Remove the .activated-heart class to make the heart appear red
+          event.target.classList.remove("activated-heart");
+//Change the heart to a full heart
+          event.target.innerHTML = EMPTY_HEART;
+        }else
+//Add the .activated-heart class
+        event.target.classList.add("activated-heart");
+//Change the heart back to an empty heart
+        event.target.innerHTML = FULL_HEART;
+
+        activated;
+      })
+
+//When the "server" returns a failure status:
+  //Display the error modal by removing the .hidden class
+      .catch(errMes => {
+        console.log(errMes);
+        errorModal.classList.remove("hidden");
+ //Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+      setTimeout((errTimeout) => {
+        errorModal.classList.add("hidden")
+         }, 3000)
+      })
+    }
+  })
+}
 
 
 //------------------------------------------------------------------------------
